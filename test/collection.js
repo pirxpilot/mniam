a434d611387429ad0eaf274f8afa95d103a8bcc0
Damian Krzeminski <pirxpilot@code42day.com>
2016-04-14 09:46:15 -0400

    add collection.eachLimit functions
    eachLimit processes all items in the collection, up to `limit` items can
    be processed in parallel (and thus the items can be processed in any
    order)
    
    this is usedful when you need to iterate over all elements in large
    collection and iterator is itself a function that yields frequently

--- test/collection.js
+++ test/collection.js
@@ -1,15 +1,19 @@
 var should = require('should');
 var database = require('../lib/database');
+var async = require('async');
 
-/*global describe, it*/
+/*global describe, it, before */
 
 describe('collection', function() {
+  before(function() {
+    this.db = database('mongodb://localhost/mniam-test');
+  });
+
   it('supports crud methods', function(done) {
-    var db = database('mongodb://localhost/mniam-test'),
-      friends = db.collection({
-        name: 'friends',
-        indexes: [[{ name: 1 }]]
-      });
+    var friends = this.db.collection({
+      name: 'friends',
+      indexes: [[{ name: 1 }]]
+    });
 
     friends.save({
       name: 'Alice',
@@ -38,4 +42,36 @@ describe('collection', function() {
       });
     });
   });
+
+
+  it('eachLimit iterates over all elements of collection', function(done) {
+    var numbers = this.db.collection({
+      name: 'numbers',
+      batchSize: 33
+    });
+    var results = [];
+    var TEST_LEN = 421;
+
+    function send(value, fn) {
+      numbers.save({ value: value }, fn);
+    }
+
+    function receive(item, fn) {
+      results[item.value] = true;
+      fn();
+    }
+
+    async.times(TEST_LEN, send, function() {
+      numbers.eachLimit(7, receive, function(err) {
+        // need all items received
+        results.should.have.length(TEST_LEN);
+        // and they all need to be true
+        results.filter(function(x) { return x; }).should.have.length(TEST_LEN);
+
+        done(err);
+      });
+    });
+
+  });
+
 });
